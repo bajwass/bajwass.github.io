84a92789cf301aa1cc181cad2ffb28f51dc83fac
bajwass <sumeet-bajwa-34@live.ca>
2021-02-26 22:52:31 -0800

    Update lab5-2.js

--- script/lab5-2.js
+++ script/lab5-2.js
@@ -1,5 +1,8 @@
 var nodes = document.getElementsByTagName("*");
 
 for(var i =0; nodes.length;i++){
-    if(nodes[i].nodeType !=3)
+    if(nodes[i].nodeType !=3){
+        var oldnodes = nodes[i].innerHTML;
+        nodes[i].innerHTML = oldnodes
+    }
 }
