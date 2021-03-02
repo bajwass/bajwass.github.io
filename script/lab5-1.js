fd883e0073e14ba2dec54ca6903b2d38f7ad5f3e
bajwass <sumeet-bajwa-34@live.ca>
2021-03-01 23:12:14 -0800

    lab5-1.js color change done

--- script/lab5-1.js
+++ script/lab5-1.js
@@ -1,5 +1,23 @@
-var color = $("#required").css("background-color");
-$("#required").css("background-color","red");
+function background(b){
+    if(b.type == "focus"){
+        b.target.style.backgroundColor = "red";
+
+    }
+    else if(b.type == "blur"){
+        b.target.style.backgroundColor = "white";
+    }
+}
+
+window.addEventListener("load", function(){
+    var cssSelect = "input[type=text], textarea[name=description]";
+    var field = document.querySelectorAll(cssSelect);
+    for(i=0; i<field.length;i++){
+        field[i].addEventListener("focus", background);
+        field[i].addEventListener("blur", background);
+    }
 
 
+});
+function prevent (p){
 
+}
