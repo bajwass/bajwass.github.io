7ab43322f1da4e3e0420a252b5ed6b2d3632db82
bajwass <sumeet-bajwa-34@live.ca>
2021-03-02 20:34:23 -0800

    updates

--- script/lab5-1.js
+++ script/lab5-1.js
@@ -1,3 +1,4 @@
+
 function background(b){
     if(b.type == "focus"){
         b.target.style.backgroundColor = "red";
@@ -18,7 +19,23 @@ window.addEventListener("load", function(){
 
 
 });
-        document.querySelector("mainForm").addEventListener("submit", function(f){
-                f.preventDefault();
-            });
+function check(c){
+    var select = "input[type=text], textarea[name=description]";
+    var cbox = "input[type=checkbox]";
+    var s = document.querySelectorAll(select);
+    var c = document.querySelectorAll(cbox);
+    
+    if(cbox.ch)
+}
+document.querySelector("mainForm").addEventListener("submit", function(e){
+    var blanks = document.querySelectorAll(".required");
+    
+    for(var i=0; i<blanks.length;i++){
+        if(check(blanks[i])){
+            e.preventDefault();
+        }
+        
+
+    }
+});
 
--- script/lab5-2.js
+++ script/lab5-2.js
@@ -4,6 +4,15 @@ window.addEventListener("load", function(){
     for(var i=1;i<nodes.length;i++){
         var nodenum = nodes[i].nodeType;
         var cnode = nodes[i];
+
+        if(nodenum !=3 && cnode.nodeName !="DIV"){
+            var o = cnode.innerHTML;
+            cnode.innerHTML = o + "<div class = 'hoveNode'>"+ cnode.nodeName + "</div>"
+            cnode.addEventListener("click",NodeF);
+        }
+    }
+    function NodeF(){
+        alert(cnode.id + cnode.parentNode.nodeName);
     }
 
 
