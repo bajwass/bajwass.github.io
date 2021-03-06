8d763a5c2776fd95a5baa37eaeafc873df421a08
bajwass <sumeet-bajwa-34@live.ca>
2021-03-05 18:44:06 -0800

    changes lab6

---
+++ script/lab6-1.js
@@ -0,0 +1,29 @@
+/* jQuery and JavaScript code here for lab6-1.html */
+
+$("#pageTitle").html("Lab6 - DOM Manipulation with jQuery.");
+
+var ta = $("").attr("");
+$("").attr("","");
+
+
+
+
+
+
+
+var bg = $("body").css("background-color"); 
+$("body").css("background-color", "ivory");
+
+$(".panel")
+    .on("click",function(){
+        $("#message").html("You clicked in panel");
+        $(".panel").off("mousemove");
+
+    })
+    .on("mousemove", function(e){
+     $("#message").html("x=" + e.pageX + " y=" + e.pageY);   
+    })
+    on("#mouseleave", function(e){
+        $("message").html("The mouse has left.")
+
+    });
