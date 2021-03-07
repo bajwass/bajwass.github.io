67c2c21929a4c398a3af6b113d3a27cb1f38ad96
bajwass <sumeet-bajwa-34@live.ca>
2021-03-06 19:45:49 -0800

    step 1-11 done Lab6-1

---
+++ js/lab6-1.js
@@ -0,0 +1,27 @@
+/* jQuery and JavaScript code here for lab6-1.html */
+
+$("#pageTitle").html("Lab 6 - DOM Manipulation with jQuery.");
+
+var ta = $("p").attr("textarea");
+$("p").attr("textarea","#msgArea");
+$("#msgArea").html("My class is form-control");
+
+$("button").css("background-color", "red");
+$("body").css("background-color", "ivory");
+
+$(".center-icons").addClass("selected");
+
+
+$(".panel")
+    .on("click",function(){
+        $("#message").html("You clicked in panel");
+        $(".panel").off("mousemove");
+    })
+    .on("mousemove", function(e){
+     $("#message").html("x=" + e.pageX + " y=" + e.pageY);   
+    })
+    .on("mouseleave", function(e){
+        $("#message").html("The mouse has left.")
+    });
+
+
